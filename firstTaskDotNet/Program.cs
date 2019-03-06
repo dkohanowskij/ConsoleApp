1890a02dd0f2ad5dbfebca3e576f270ae5e33ab4
Dzmitriy <Dzmitriy_Kakhanouski@epam.com>
2019-03-06 10:58:47 +0300

    add

---
+++ firstTaskDotNet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TestLib;
+
+namespace firstTaskDotNet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      Console.WriteLine("Enter you name:");
+      var name = Console.ReadLine();
+
+      UserManager userManager = new UserManager(name);
+
+      Console.WriteLine(userManager.GetGreeting());
+      Console.ReadKey();
+    }
+  }
+}
